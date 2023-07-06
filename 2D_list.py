6d21612c96083cb9db18f1b5d3580ef3c8bb3710
Leon Li <gn01742053@gmail.com>
2023-07-06 18:36:20 +0800

    add read_file.py (if exists read file and split with comma.)

--- 2D_list.py
+++ 2D_list.py
@@ -5,7 +5,7 @@ while True:
         break
     price = input('please enter product price: ')
     products.append([name, price])
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('item,price\n')
     for p in products:
         f.write(p[0] + ',' + p[1] + '\n')
